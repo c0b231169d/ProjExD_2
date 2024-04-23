3808c961e8a6eb1f7a174229472ceb41ff42cb2a
hamayamei <c0b231169d@edu.teu.ac.jp>
2024-04-23 17:32:31 +0900

    演習1：こうかとん画像途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,11 +5,15 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-DELTA = {  #移動量辞書
+DELTA = {  # 移動量辞書
     pg.K_UP: (0, -5), 
     pg.K_DOWN: (0, 5), 
     pg.K_LEFT: (-5, 0), 
     pg.K_RIGHT: (5, 0)
+}
+KKMUKI = {  # 移動量の合計値辞書
+    (5, 0):"kk_img", (5, 5):" kk_img2", (0, 5):" kk_img3", (-5, 5):" kk_img4", 
+    (-5, 0):" kk_img5", (-5, -5):" kk_img6", (0, -5):" kk_img7", (5, -5):" kk_img8"
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,12 +31,18 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_img2 = pg.transform.rotozoom(pg.image.load("fig/3.png"), -45, 2.0)
+    kk_img3 = pg.transform.rotozoom(pg.image.load("fig/3.png"), -90, 2.0)
+    kk_img7 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 90, 2.0)
+    kk_img8 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0)
+    kk_img4 = pg.transform.flip(kk_img2, True, False)
+    kk_img5 = pg.transform.flip(kk_img, True, False)
+    kk_img6 = pg.transform.flip(kk_img8, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bakudan_img = pg.Surface((20, 20))
@@ -41,14 +51,12 @@ def main():
     bakudan_rct = bakudan_img.get_rect()
     bakudan_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5  #横移動速度、縦移動速度
-
     blackout_img = pg.Surface((WIDTH, HEIGHT))
     pg.draw.rect(blackout_img, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
     blackout_img.set_alpha(127)
     fonto = pg.font.Font(None, 80)
     go_txt = fonto.render("Game Over", True, (255, 255, 255))
     kkcry_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)
-
     clock = pg.time.Clock()
     tmr = 0
 
@@ -76,7 +84,7 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img, kk_rct)  # こうかとんの画像を表示
         bakudan_rct.move_ip(vx, vy)
         screen.blit(bakudan_img, bakudan_rct)
         yoko, tate = check_bound(bakudan_rct)
